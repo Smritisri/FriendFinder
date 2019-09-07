d8f3b592cfb7866a14e387389877a633beabd3ba
Smriti Sri <smritis2001@gmail.com>
2019-09-07 09:40:30 -0500

    changes frontend and routes

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.static('app/public'))
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-// require("./app/routing/apiRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
